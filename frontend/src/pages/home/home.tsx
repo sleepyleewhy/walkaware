bed17ab8d2244c34dd34260052964ee6e12acf7c
Endl Levente <levente.endl@stud.uni-obuda.hu>
2025-04-09 19:19:57 +0200

    moved div to layout

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -5,14 +5,15 @@ import { Button } from "@/components/ui/button";
 const Home : React.FC = () => {
     const navigate = useNavigate();
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+            <>
             <h1 className="text-5xl font-extrabold mb-10">Walk Help</h1>
             <div className="flex items-center">
             <Button className="mr-4"onClick={() => navigate('/pedestrian')}>Pedestrian Mode</Button>
             <Button  className="">Driver Mode</Button>
             </div>
+            </>
             
-        </div>
+
     )
 }
 
