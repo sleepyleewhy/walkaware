0b6b04f719e78f742f41255d172e6d5d0acfe7d8
Endl Levente <levente.endl@stud.uni-obuda.hu>
2025-04-09 18:43:43 +0200

    home page basic look

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -5,9 +5,13 @@ import { Button } from "@/components/ui/button";
 const Home : React.FC = () => {
     const navigate = useNavigate();
     return (
-        <div>
-            <h1>Home Page</h1>
-            <Button onClick={() => navigate('/pedestrian')}>Pedestrian Page</Button>
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+            <h1 className="text-5xl font-extrabold mb-10">Walk Help</h1>
+            <div className="flex items-center">
+            <Button className="mr-4"onClick={() => navigate('/pedestrian')}>Pedestrian Mode</Button>
+            <Button  className="">Driver Mode</Button>
+            </div>
+            
         </div>
     )
 }
