82541139ad49537c9a3fe6593a491b4cab04b0d2
Endl Levente <levente.endl@stud.uni-obuda.hu>
2025-10-03 16:44:15 +0200

    making progress with communication ű

--- frontend/src/context/driverProvider.tsx
+++ frontend/src/context/driverProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import { DriverContext } from "./driverContext";
 import useLocation from "@/hooks/sensors/useLocation";
 import { DriverContextType } from "@/models/driverContextType";
@@ -7,7 +7,7 @@ import useRelevantCrosswalkSearcher from "@/hooks/services/useRelevantCrosswalkS
 import { CrosswalkCoordinates } from "@/models/crosswalkCoordinates";
 import useCrosswalkDistanceWatcher from "@/hooks/services/useCrosswalkDistanceWatcher";
 import useDriverCommuncicator from "@/hooks/services/useDriverCommunicator";
-import { socket } from "./socketContext";
+import {useSocketContext } from "./socketContext";
 
 
 type DriverProviderProps = {
@@ -21,11 +21,37 @@ const DriverProvider : React.FC<DriverProviderProps> = ({ children }) => {
     const {location, setLocation, isLocationActive, setIsLocationActive} = useLocation(alertLevel, locationDebug);
     const [dangeredCrosswalks, setDangeredCrosswalks] = useState<CrosswalkCoordinates[]>([]);
     const [relevantCrosswalks, setRelevantCrosswalks] = useState<CrosswalkCoordinates[]>([]);
+    const socket = useSocketContext();
 
     useAccuracyChecker(location, alertLevel, setAlertLevel);
     useRelevantCrosswalkSearcher(location, alertLevel, setAlertLevel, setRelevantCrosswalks);
     useCrosswalkDistanceWatcher(alertLevel, setAlertLevel, relevantCrosswalks, setDangeredCrosswalks, location)
     useDriverCommuncicator(dangeredCrosswalks, socket, alertLevel, setAlertLevel)
+    const alertLevelRef = useRef(alertLevel);
+    useEffect(() => {
+        alertLevelRef.current = alertLevel;
+    }, [alertLevel])
+
+    useEffect(() => {
+        socket.on("presence" , (data: { pedestrian_count: number }) => {
+            console.log("[driver_communicator] presence", data);
+        })
+        socket.on("driver_critical", () => {
+            if (alertLevelRef.current == 3) {
+                setAlertLevel(4);
+                console.log("[driver_communicator] driver_critical received");
+            }
+        })
+        socket.on("alert_end", () => {
+            if (alertLevelRef.current === 4) {
+                setAlertLevel(3);
+            }
+            console.log("[driver_communicator] alert_end received");
+        })
+
+    }, [socket])
+
+
 
     const contextValue: DriverContextType = {
         location,
