f893029bb6cdd3eea5694040258b614dbd8de94e
Endl Levente <levente.endl@stud.uni-obuda.hu>
2025-10-18 14:33:27 +0200

    pedestrian page layout change

--- frontend/src/pages/pedestrian/pedestrianPage.tsx
+++ frontend/src/pages/pedestrian/pedestrianPage.tsx
@@ -6,6 +6,8 @@ import StartPedestrianButton from "@/components/pedestrian/StartPedestrianButton
 import PedestrianAlertHUD from "@/components/pedestrian/PedestrianAlertHUD";
 import { usePedestrianContext } from "@/context/pedestrianContext";
 import { Label } from "@/components/ui/label";
+import { Footprints } from "lucide-react";
+import { Switch } from "@/components/ui/switch";
 const PedestrianPage: React.FC = () => {
 
 
@@ -13,14 +15,14 @@ const PedestrianPage: React.FC = () => {
         const { allowImageStorage, setAllowImageStorage } = usePedestrianContext();
         return (
             <div className="flex items-center space-x-2">
-                <input
+                <Switch
                     id="allowImageStorage"
-                    type="checkbox"
+                    title="allow image storing"
                     checked={allowImageStorage}
-                    onChange={(e) => setAllowImageStorage(e.target.checked)}
+                    onCheckedChange={(checked) => setAllowImageStorage(Boolean(checked))}
                 />
                 <Label htmlFor="allowImageStorage">
-                    Allow saving images to improve crosswalk detection
+                    Allow saving camera images for future model training to improve crosswalk detection
                 </Label>
             </div>
         );
@@ -28,9 +30,10 @@ const PedestrianPage: React.FC = () => {
 
     return (
         <PedestrianProvider>
-            <h1 className="font-extrabold text-4xl mb-4">Pedestrian Mode</h1>
-            <div className="flex flex-col space-y-4 mb-5">
-                <div className="flex space-x-5">
+            <div className="flex items-center mb-3">
+                <Footprints className="mr-2 size-5" /> <h1 className="font-extrabold text-2xl">Pedestrian Mode</h1></div>
+            <div className="flex flex-col space-y-3 mb-4">
+                <div className="flex gap-3">
                     <StartPedestrianButton/>
                     <PedestrianCalibrationDrawer />
                 </div>
